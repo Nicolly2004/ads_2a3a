dea2da74a98f598fdaacc09b3b9790611018e46f
Fernanda Torres <fernanda.rodriguestorres70@gmail.com>
2024-04-16 10:29:46 -0300

    Final aula 10

--- Projeto_SMP/app.js
+++ Projeto_SMP/app.js
@@ -1,9 +1,13 @@
 //fazer  importação do express
 const express = require('express');
 const router = require('./routes/index');
+
 //configurações básicas do app
 const app = express();
 app.use('/', router);
+
+
+app.use (express.json());
 
 //exportando o app, pois iremos importá-los no servidor
 module.exports = app;
